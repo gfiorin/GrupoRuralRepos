74ed322635881b54c607bf2bbe025fd11d4be7ae
gfiorin <gfiorina@correo.um.edu.uy>
2022-09-16 12:04:57 -0300

    .

---
+++ GrupoRural/src/main/java/com/UM/GrupoRural/GrupoRuralApplication.java
@@ -0,0 +1,13 @@
+package com.UM.GrupoRural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoRuralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoRuralApplication.class, args);
+	}
+
+}
